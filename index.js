d2210a6963b1192af7fdea55b1f4d9ed2e7f83fd
tkadima <kadima.t@husky.neu.edu>
2021-02-17 10:42:47 -0500

    trying to figure out insomnia

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = requires('express')
+const express = require('express')
 const cors = require('cors')
 
 const app = express()
@@ -7,7 +7,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cors())
 
-app.use('/v1/FaveChars', require('./contollers/v1/bounties'))
+app.use('/v1/FaveChars', require('./controllers/v1/characters'))
 
 app.get('*', (req, res) => {
     res.status(404).send({message: 'NOT FOUND!'})
