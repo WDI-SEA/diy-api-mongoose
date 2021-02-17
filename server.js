8e440ee33dec6995e75ca3e48e7bdf53f776d976
kys3923 <kys3923@gmail.com>
2021-02-16 20:25:48 -0500

    starting the homework, done with server.js part 1

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Middleware
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Mongoose
+mongoose.connect(
+    'mongodb://localhost/acnhWidgets',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+
+// Connection methods
+db.once('open', () => {
+    console.log(`🔗 Connected to MongoDb at ${db.host}:${db.port}`);
+});
+
+db.on('error', err => {
+    console.error(`Database error : ${err}`);
+});
+
+// controllers
+app.use('/widgets', require('./controllers/widgets'));
+
+app.listen(3000 || process.env.PORT, () => console.log(`You're listening to the smooth sounds of port
+ ${3000 || process.env.PORT} 💲`))
