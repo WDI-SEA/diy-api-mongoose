6a67b2394b10a68a38e204b2cedf5b68e47e5406
sameghosts <seanfeiner.brown@gmail.com>
2021-02-17 07:16:07 -0800

    model created

---
+++ models/synth.js
@@ -0,0 +1,30 @@
+// req mongoose
+const MONGOOSE = require('mongoose');
+
+// create a schema
+const synthSchema = new MONGOOSE.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  manufacturer: {
+    type: String,
+    required: true
+  },
+  kind: {
+    type: String,
+    required: true
+  },
+  releaseYear: Number,
+  notableArtists: [String],
+}, {
+  timestamps: true
+});
+
+// methods
+
+// name model
+const Synth = mongoose.model('Synth', synthSchema);
+
+// export the model 
+module.exports = Synth;
