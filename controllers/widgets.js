87be5216a01776a4120992e337a649c1f3c5c0a1
kys3923 <kys3923@gmail.com>
2021-02-16 20:55:57 -0500

    CRUD function done

--- controllers/widgets.js
+++ controllers/widgets.js
@@ -3,7 +3,7 @@ const Widget = require('../models/widget');
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
+router.get('/widgets', (req, res) => {
     Widget.find({}, (err, widgets) => {
         if (err) {
             console.error(`Error in widgets Index route: ${err}`);
@@ -13,5 +13,49 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/widgets/:id', (req, res) => {
+    Widget.findById(req.params.id, (err, widget) => {
+        if (err) {
+            console.error(`Error in widgets Detail route: ${err}`);
+            return res.status(500).json({ error: 'Error in widgets Detail route'});
+        }
+        res.json({ widget });
+    });
+});
+
+router.post('/widgets', (req, res) => {
+    Widget.create(req.body, (err, widget) => {
+        if (err) {
+            console.error(`Error in widgets Create route: ${err}`);
+            return res.status(500).json({ error: 'Error in widgets Create route'});
+        }
+        res.json({ widget });
+    });
+});
+
+router.put('/widgets/:id', (req, res) => {
+    Widget.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (err, widget) => {
+            if (err) {
+                console.error(`Error in widgets Update route: ${err}`);
+                return res.status(500).json({ error: 'Error in widgets Update route'});
+            }
+            res.json({ widgets });
+        }
+    );
+});
+
+router.delete('/widgets/:id', (req, res) => {
+    Widget.findByIdAndDelete(req.params.id, (err, widget) => {
+        if (err) {
+            console.error(`Error in widgets Delete route: ${err}`);
+            return res.status(500).json({ error: 'Error in widgets Delete route'});
+        }
+        res.json({ deletedWidget: widget });
+    })
+})
 
 module.exports = router;
