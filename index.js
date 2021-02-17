48c8865c9bcf528e3f6a3311e431451006b01572
tkadima <kadima.t@husky.neu.edu>
2021-02-16 20:44:01 -0500

    adding mongoose event handlers

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.use(express.urlencoded({ extended: false}))
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/faveCharacters')
 
+//shorcut to mongoose.connection object
+const db = mongoose.connection
+
+db.once('open', function(){
+    console.log(`Connected to MongoDB at ${port}`)
+})
+
+db.on('error', function(err){
+    console.log(`Database error:${err}`)
+})
+
 app.get('/', function(req, res) {
     res.send('Hi')
 })
