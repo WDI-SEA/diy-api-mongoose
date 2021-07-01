cca938aedb5fdc62c03876a4e9d6995d804148a9
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-01 18:03:14 -0500

    Basic routes written.

--- index.js
+++ index.js
@@ -13,27 +13,67 @@ const log = console.log
 // ROUTES - - - - - - - - - - - - - - - - -
 // INDEX /blog -- list all blog posts
 app.get('/blog', (req, res) => {
-    res.json({ msg: "List all blog posts" })
+    db.Blog.find({})
+    .then((result) => {
+        res.json(result)
+    })
+    .catch(err => {
+        log(err)
+    })
 })
 
 // POST /blog -- add new blog post
 app.post('/blog', (req, res) => {
-    res.json({ msg: "Add new blog post" })
+    db.Blog.create({
+        name: req.body.name,
+        title: req.body.title,
+        content: req.body.content
+    })
+    .then(() => {
+        res.redirect('/blog')
+    })
+    .catch(err => {
+        log(err)
+    })
 })
 
 // GET /blog/:id -- show one blog post
 app.get('/blog/:id', (req, res) => {
-    res.send("Show one blog post")
+    db.Blog.findById(req.params.id)
+    .then(() => {
+        res.redirect(`/blog/${req.params.id}`)
+    })
+    .catch(err => {
+        log(err)
+    })
 })
 
 // PUT /blog/:id -- update one blog post
 app.put('/blog/:id', (req, res) => {
-    res.send("Update one blog post")
+    db.Blog.findById(req.params.id)
+    .then((foundPost) => {
+        foundPost.name = req.body.name
+        foundPost.title = req.body.title
+        foundPost.content = req.body.content
+        foundPost.save()
+        .then(() => {
+            res.redirect(`/blog/${req.params.id}`)
+        })
+    })
+    .catch(err => {
+        log(err)
+    })
 })
 
 // DELETE /blog/:id -- delete one blog post
 app.delete('/blog/:id', (req, res) => {
-    res.send("Delete one blog post")
+    db.Blog.findByIdAndDelete(req.params.id)
+    .then(() => {
+        res.redirect('/blog')
+    })
+    .catch(err => {
+        log(err)
+    })
 })
 
 // LISTEN - - - - - - - - - - - - - - - - -
