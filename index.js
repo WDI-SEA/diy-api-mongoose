bc7d98e5bba7c39283488fcbdf2e2e9b4e4f0c0f
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-01 17:55:21 -0500

    Routes stubbed out, tested on Postman.

--- index.js
+++ index.js
@@ -12,14 +12,29 @@ const log = console.log
 
 // ROUTES - - - - - - - - - - - - - - - - -
 // INDEX /blog -- list all blog posts
+app.get('/blog', (req, res) => {
+    res.json({ msg: "List all blog posts" })
+})
 
 // POST /blog -- add new blog post
+app.post('/blog', (req, res) => {
+    res.json({ msg: "Add new blog post" })
+})
 
 // GET /blog/:id -- show one blog post
+app.get('/blog/:id', (req, res) => {
+    res.send("Show one blog post")
+})
 
 // PUT /blog/:id -- update one blog post
+app.put('/blog/:id', (req, res) => {
+    res.send("Update one blog post")
+})
 
 // DELETE /blog/:id -- delete one blog post
+app.delete('/blog/:id', (req, res) => {
+    res.send("Delete one blog post")
+})
 
 // LISTEN - - - - - - - - - - - - - - - - -
 app.listen(PORT, () => {
