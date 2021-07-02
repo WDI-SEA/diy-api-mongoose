fbdf0e9c4a091b3866944ea71691a3f4c0cac236
hamazumi <khamamoto12@gmail.com>
2021-07-01 22:34:39 -0700

    changed all to async functions

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ db.connect()
 
 //config our express app
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 //request body middleware
 app.use(express.urlencoded({ extended: false }))
@@ -29,44 +29,104 @@ app.get('/blog', async (req, res) => {
 })
 
 //POST /blog -- CREATE one post then redirect to /blog
-app.post('/blog', (req, res) => {
-    db.Post.create({
-        name: req.body.name,
-        title: req.body.title ,
-        content: req.body.content
-    })
-    .then(() => {
+// app.post('/blog', (req, res) => {
+//     db.Post.create({
+//         name: req.body.name,
+//         title: req.body.title ,
+//         content: req.body.content
+//     })
+//     .then(() => {
+//         res.redirect('/blog')
+//     })
+//     .catch(err => console.log(err))
+// })
+
+//changed /POST to async
+app.post('/blog', async (req, res) => {
+    try{
+        const newPost = await db.Post.create({
+            name: req.body.name,
+            title: req.body.title ,
+            content: req.body.content
+        })
+        await newPost.save()
         res.redirect('/blog')
-    })
-    .catch(err => console.log(err))
+    }catch(err){
+        console.log(err)
+    }
 })
+
+//GET show one blog post
+app.get('/blog/:id', async (req, res) => {
+    try{
+        const singlePost = await db.Post.findById(req.params.id)
+        if(singlePost) {
+            res.send(singlePost)
+        } else {
+            res.send({
+                message: "Post not Found"
+            })
+        }
+    }catch(err) {
+        console.log(error)
+    }
+})
+
+
 
 //PUT /blog:id -- UPDATE one bountie and redirect to /blog
-app.put('/blog/:id', (req, res) => {
-    db.Post.findById(req.params.id)
-    .then(foundPost => {
-        foundPost.name = req.body.name
-        foundPost.title = req.body.title,
-        foundPost.content = req.body.content,
+// app.put('/blog/:id', (req, res) => {
+//     db.Post.findById(req.params.id)
+//     .then(foundPost => {
+//         foundPost.name = req.body.name
+//         foundPost.title = req.body.title,
+//         foundPost.content = req.body.content,
         
 
-        foundPost.save()
-        .then(() => {
-            res.redirect('/blog')
-        })
-        .catch(err => console.log(err))
-    })
-    .catch(err => console.log(err))
+//         foundPost.save()
+//         .then(() => {
+//             res.redirect('/blog')
+//         })
+//         .catch(err => console.log(err))
+//     })
+//     .catch(err => console.log(err))
 
+// })
+
+//changed /PUT to async
+app.put('/blog/:id', async (req, res) => {
+    try{
+        const updatePost = await db.Post.findById(req.params.id)
+            updatePost.name = req.body.name
+            updatePost.title = req.body.title,
+            updatePost.content = req.body.content
+
+            updatePost.save()   
+            res.redirect('/blog')         
+    }catch(err){
+        console.log(err)
+    }
 })
 
+
 //DELETE /blog/:id -- DESTROY one post and redirect to /blog
-app.delete('/blog/:id', (req, res) => {
-    db.Post.findByIdAndDelete(req.params.id)
-        .then(deletedItem => {
-            console.log(deletedItem)
-            res.redirect('/blog')
-        })
+// app.delete('/blog/:id', (req, res) => {
+//     db.Post.findByIdAndDelete(req.params.id)
+//         .then(deletedItem => {
+//             console.log(deletedItem)
+//             res.redirect('/blog')
+//         })
+// })
+
+//changed /DELETE to async
+app.delete('/blog/:id', async (req, res) => {
+    try{
+        deletePost = await db.Post.findByIdAndDelete(req.params.id)
+        console.log(deletePost)
+        res.redirect('/blog')
+    }catch(err){
+        console.log(err)
+    }
 })
 
 app.listen(PORT, () => console.log('port working!!'))
