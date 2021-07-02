27b0d7d9bd3c33741a74936d06197185be264f49
Consecratio <jcasique@gmail.com>
2021-07-02 13:47:04 -0700

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 // require express
 const express = require('express')
+const cors = require('cors')
 
 // require rowdy
 const rowdy = require('rowdy-logger')
@@ -17,6 +18,7 @@ const PORT = process.env.PORT
 const rowdyResults = rowdy.begin(app)
 
 // configure middlewares
+app.use(cors())
 app.use('/blog', require('./controllers/v1/blog'))
 
 // app listen
