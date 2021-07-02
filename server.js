952cdc11b434d8a3f82c35f033465efa15b2fda7
nesbita <ariananesbit@gmail.com>
2021-07-02 11:01:15 -0600

    moved stuff around, still not working

--- server.js
+++ server.js
@@ -1,15 +1,82 @@
-// this page has the mongo connection stuff before hitting the mongo db
-
 const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
-
+const db = require('./models')
 const app = express()
+
+db.connect()
+
 PORT = 3001
 app.use(express.urlencoded({extended: false}))
-app.use(express.json())
 
-app.use('/Blog', require('./models'))
+Blog.create({
+  title: 'This is a blog post',
+  author: 'Ben'
+}, (error, Blog) => {
+  if(error){
+    console.log(error)
+  } else {
+    console.log(blog.title)
+  }
+}) 
+
+// GET (index) route for /Blog.  READ all blog posts.
+app.get('./Blog', async (req, res) => {
+    try {
+        const blogs = await db.Blog.find({})
+        res.json({ blogs })
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+// POST (create) route for blog.  CREATE one new blog post.
+app.post('/Blog', (req, res) => {
+    db.Blog.create({
+      title: req.body.title,
+      author: req.body.author,
+      published: req.body.published,
+    })
+    .then(() => {
+      res.redirect('/Blog')
+    })
+    .catch(err => console.log(err))
+})
+
+// GET (detail/show) /blog/:id route.  Show one blog post
+app.get('/Blog/:id', (req, res) => {
+    db.Blog.findById(req.params.id)
+      .then(foundBlog => {
+          foundBlog.title = req.body.title
+          
+          foundBlog.save()
+          .then(() => {
+            res.redirect('/blog')
+          })
+      })
+})  
+    
+  // PUT route for /blog/:id. UPDATE one blog post.
+app.put('/Blog/:id', (req, res) => {
+    db.Blog.findById(req.params.id)
+      .then(foundBlog => {
+        foundBlog.title = req.body.title
+  
+        foundBlog.save()
+          .then(() => {
+            res.redirect('/Blog')
+          })
+      })
+})
+
+// DELETE route for /blog/:id.  Deletes one blog post.
+app.delete('/Blog/:id', (req, res) => {
+    db.Blog.findByIdAndDelete(req.params.id)
+      .then(deletedItem => {
+        console.log(deletedItem)
+        res.redirect('/Blog')
+      })
+  })
+
+
 app.listen (3001, () => {
     console.log('listening')
-})
+  })
