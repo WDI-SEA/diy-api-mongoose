bc028016c180f01885e3507ba8c4bd8daa4de116
Consecratio <jcasique@gmail.com>
2021-07-02 10:08:56 -0700

    refactored routes into controller

---
+++ controller/blog.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+
+// configure middlewares
+router.use(express.urlencoded({ extended: false }))
+
+// GET /blog -- READ list all blog posts
+router.get('/', async (req, res) => {
+    try {
+        const allPosts = await db.Post.find()
+        res.json({ allPosts })
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// POST /blog -- CREATE add a new blog post, redirect to /blog
+router.post('/', async (req, res) => {
+    try {
+        await db.Post.create({
+            author: req.body.author,
+            title: req.body.title,
+            content: req.body.content,
+            tags: req.body.tags
+        })
+        res.redirect('/blog')
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// GET /blog/:id -- READ show one blog post
+router.get('/:id', async (req, res) => {
+    try {
+        const foundPost = await db.Post.find({
+            _id: req.params.id
+        })
+        res.json({ foundPost })
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// PUT /blog/:id -- UPDATE one blog post, and redirect to /blog
+router.put('/:id', async (req, res) => {
+    try {
+        const postToUpdate = await db.Post.findById(req.params.id)
+        postToUpdate.content = req.body.content
+        await postToUpdate.save()
+
+        res.redirect('/blog')
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// DELETE /blog/:id -- DESTROY one blog post, and redirect to /blog
+router.delete('/:id', async (req, res) => {
+    try {
+        await db.Post.findByIdAndDelete(req.params.id)
+
+        res.redirect('/blog')
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -17,69 +17,7 @@ const PORT = process.env.PORT
 const rowdyResults = rowdy.begin(app)
 
 // configure middlewares
-app.use(express.urlencoded({ extended: false }))
-
-// create routes
-// GET /blog -- READ list all blog posts
-app.get('/blog', async (req, res) => {
-    try {
-        const allPosts = await db.Post.find()
-        res.json({ allPosts })
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-// POST /blog -- CREATE add a new blog post, redirect to /blog
-app.post('/blog', async (req, res) => {
-    try {
-        await db.Post.create({
-            author: req.body.author,
-            title: req.body.title,
-            content: req.body.content,
-            tags: req.body.tags
-        })
-        res.redirect('/blog')
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-// GET /blog/:id -- READ show one blog post
-app.get('/blog/:id', async (req, res) => {
-    try {
-        const foundPost = await db.Post.find({
-            _id: req.params.id
-        })
-        res.json({ foundPost })
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-// PUT /blog/:id -- UPDATE one blog post, and redirect to /blog
-app.put('/blog/:id', async (req, res) => {
-    try {
-        const postToUpdate = await db.Post.findById(req.params.id)
-        postToUpdate.content = req.body.content
-        await postToUpdate.save()
-
-        res.redirect('/blog')
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-// DELETE /blog/:id -- DESTROY one blog post, and redirect to /blog
-app.delete('/blog/:id', async (req, res) => {
-    try {
-        await db.Post.findByIdAndDelete(req.params.id)
-
-        res.redirect('/blog')
-    } catch (err) {
-        console.log(err)
-    }
-})
+app.use('/blog', require('./controller/blog'))
 
 // app listen
 app.listen(PORT, () => {
