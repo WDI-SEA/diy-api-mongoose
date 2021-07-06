e9fae99974d801c91dad1f0586b8401e48d60b8d
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-06 17:51:15 -0500

    Added express body parser.

--- index.js
+++ index.js
@@ -2,15 +2,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const bodyParser = require("body-parser")
-
 const db = require('./models')
 db.connect()
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 
 const PORT = 4000
 const log = console.log
