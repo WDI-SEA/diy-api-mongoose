3c6fcba3cf037ee02c84de29a3261a2512608c38
galyverasi <galyverasi@gmail.com>
2021-12-08 20:04:35 -0700

    connected to Mongo

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
+const db = require('./models')
 
 app.use(express.urlencoded({extended:false}))
+
+// middleware
+app.use('/blog', require('./controllers/blog'))
 
 app.use('/', (req, res) => {
     res.json({message: 'Blog Home Route'})
 })
 
 app.listen(process.env.PORT || 8000, () => {
-    console.log('Listening to port 8000 ✍🏻')
+    console.log('Writing to port 8000 ✍🏻')
 })
---
+++ models/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/blog')
+
+const db = mongoose.connection
+
+db.once('open', () => {
+    console.log('Connected to MongoDB at ${db.host}:${db.port}')
+})
+
+db.on('error', () => {
+    console.error('Database Error!', err)
+})
+
+module.exports.Blog = require('./blog')
