a0332dabf9bb98bd4a7a98eac1f69a2102510a3d
Norman Teodoro <normanteodoro@hotmail.com>
2022-03-17 19:30:50 -0700

    finished deliverable

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const db = require('./models')
 const PORT = 8000
 
 
 // body parser middleware
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
+app.use('/blog', require('./controllers/blog'))
 
 app.listen(PORT, ()=> {
     console.log(`Server listening at port : ${PORT}`)
