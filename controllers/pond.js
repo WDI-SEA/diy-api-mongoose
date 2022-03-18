290cb5a44f5daaa4fa19807f8c57aa9e7cc442c1
snacksident <leak187@gmail.com>
2022-03-17 19:37:29 -0700

    complete basic REST routes

--- controllers/pond.js
+++ controllers/pond.js
@@ -2,9 +2,14 @@ const db = require('../models')
 const router = require('express').Router()
 
 //GET /pond (index) - show all ponds
-router.get('/',(req,res)=>{
+router.get('/',async (req,res)=>{
     //list all ponds
-        res.json(ponds)
+    try {
+        const allPonds = await db.Pond.find({})
+        res.json(allPonds)
+    } catch (error) {
+        console.log(error)
+    }
 })
 //POST /pond (index) add a new pond)
 router.post('/', async (req,res)=>{
@@ -18,16 +23,36 @@ router.post('/', async (req,res)=>{
     res.json({message: `you've hit the post route for the pond`})
 })
 //GET /pond/:id show one pond
-router.get('/:id',(req,res)=>{
-    res.json({message: `you've made it to specific pond`})
+router.get('/:id',async (req,res)=>{
+    try {
+        const { id } = req.params
+        const foundPond = await db.Pond.findById(id)
+        res.json(foundPond)
+    } catch (error) {
+        console.log(error)
+    }
 })
 //PUT /pond/:id update one pond
-router.put('/:id',(req,res)=>{
-    res.json({message: `you've made it to the update pond route`})
+router.put('/:id',async (req,res)=>{
+    try {
+        const { id } = req.params
+        const options = {new: true}
+        const foundPond = await db.Pond.findByIdAndUpdate(id,req.body,options)
+        res.json(foundPond)
+    } catch (error) {
+        console.log(error)
+    }
+
 })
 //DELETE /pond/:id (index - lists all ponds)
-router.delete('/:id',(req,res)=>{
-    res.json({message: `attempting a delete`})
+router.delete('/:id',async (req,res)=>{
+    try {
+        const { id } = req.params
+        const foundPond = await db.Pond.findByIdAndDelete(id)
+        res.status(204).json({message: 'it worked - the bounty is deleted'})
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router
