470596c605cc107859af0ce9140bd212cddd0402
marzo20 <myjune91@gmail.com>
2022-06-19 16:51:26 -0700

    part1 done

--- controllers/car.js
+++ controllers/car.js
@@ -1,1 +1,81 @@
 const router = require('express').Router()
+const { json } = require('express')
+const db = require('../models')
+
+//  GET /cars -- index of cars
+router.get('/', async (req, res) => {
+    try {
+        //  find all cars
+        const cars = await db.Car.find({})
+        // send to the client
+        res.json(cars)
+    } catch(err){
+        res.status(500).json({ msg: 'server error'})
+    }
+})
+
+// GET /cars/:id -- details on a car
+
+router.get('/:id', async (req, res) => {
+    try{
+        const car = await db.Car.findById(req.params.id)
+    res.json(car)
+    }catch (err) {
+        res.status(500).json({ msg: 'server error'})
+    }
+})
+// POST /cars --create cars
+
+router.post('/:id', async (req, res) => {
+    try {
+        const newCar = await db.Car.create(req.body)
+        res.status(201).json(newCar)
+    }catch(err){
+        console.log('post error',err)
+        if (err.name === "ValidationError"){
+            res.status(400).json({ msg: err.message })
+        }
+        res.status(500).json({ msg: 'server error'})
+    }
+})
+
+// put /cars/:id -- update cars
+
+router.put('/:id', async (req, res)=> {
+    try {
+        // get id from the url params
+        const id = req.params.id
+        // search for the id in the db, and update using the req.body
+        const options = { new:true }
+        const updatedCar = await db.Car.findByIdAndUpdate(id, req.body, options)
+        // send the updated car back to client
+        res.json(updatedCar)
+    }catch (err) {
+        console.log('post error',err)
+        if (err.name === "ValidationError"){
+            res.status(400).json({ msg: err.message })
+        }
+        res.status(500).json({ msg: 'server error'})
+    }
+})
+
+// DELETE /cars/:id --delete cars
+
+router.delete('/:id', async (req, res) => {
+    try {
+        // get the id from params
+        const id = req.params.id
+        // delete that car in the db
+        await db.Car.findByIdAndDelete(id)
+        // send 'no content' status
+        res.sendStatus(204) // was successful --nothing exists
+    } catch(err){
+        console.log('post error',err)
+        if (err.name === "ValidationError"){
+            res.status(400).json({ msg: err.message })
+        }
+        res.status(500).json({ msg: 'server error'})
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // required packages
 const express = require('express')
-
+const cors = require('cors')
 // connect to mongo
 require('./models')
 // app config/middlewares
@@ -8,13 +8,14 @@ const app = express()
 const PORT = 8000
 // parse json request bodies
 app.use(express.json())
+app.use(cors())
 
 // routes/controllers
-app.get('.',(req, res) => {
+app.get('/',(req, res) => {
     res.json({ msg: 'DIY API'})
 })
 
-// app.use('/bounties', require('./controllers/'))
+app.use('/cars', require('./controllers/car'))
 
 // listening on a port
 app.listen(PORT, () => console.log('Listening on port', PORT))
