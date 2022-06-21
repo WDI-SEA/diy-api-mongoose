9e83cb451edc04cf79b5786505e88d13da29b36e
Hoan Le <hoanleecr@gmail.com>
2022-06-20 19:39:22 -0700

    edit some controller type

--- controllers/superherotype.js
+++ controllers/superherotype.js
@@ -1,6 +1,20 @@
 const db = require('../models')
 const router = require('express').Router()
 
+
+
+router.get('/', async (req,res) => {
+    try {
+        const heroType = await db.Superhero.create({
+            villain: false,
+        
+        })
+        res.json(heroType)
+    } catch(err) {
+        console.warn(err)
+    }
+})
+
 router.put('/:id', async (req,res) => {
     try {
         const updateHeroType = await db.Superhero.findByIdAndUpdate(req.params.id).populate('type')
