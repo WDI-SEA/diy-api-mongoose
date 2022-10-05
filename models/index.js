dd7b9ee002b9b2e916e89bd596e9c6df4df84c97
tylerchan33 <tylerchan33@gmail.com>
2022-10-04 19:28:01 -0700

    done with part 1

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const uri = "mongodb://127.0.0.1/blogs"
+mongoose.connect(uri)
+const db = mongoose.connection
+
+db.once("open", () => console.log(`mongoDB connected at ${db.host}:${db.port}`))
+db.on("error", (err) => console.warn("the data center is not having it", err))
+module.exports = {
+    Blog: require("./Blog")
+}
