c3a59986c864b9d9ac8fecf824959ffb42bba0c0
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-04 20:17:43 -0700

    cont express setup.

--- intro.js
+++ intro.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 11300
+require('./models')
+
+// important middleware.
+app.use(cors())
+app.use(express.json())
+
+app.use('/character', require('./controllers/Characters.js'))
 
 app.get('/', (req,res) => {
     res.json({message: 'home reached.'})
 })
 
-app.use('/character', require('./controllers/Characters.js'))
 
 
-app.listen(PORT, console.log())
+app.listen(PORT, () => console.log(`welcome to the party @ ${PORT}`))
