6a3c2ade62790dfd2f72b578168587854ea64a71
syorn96 <syorn2014@gmail.com>
2022-10-04 18:29:52 -0700

    stubs out pokemoncards routes

---
+++ controllers/pokemoncards.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+
+// GET /pokemoncards
+
+// GET /pokemoncards/:id
+
+// POST /pokemoncards
+
+// PUT /pokemoncards/:id
+
+// DELETE /pokemoncards/:id
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,28 @@
+// require packages
+const express = require ('express')
+const cors = require('cors')
+
+// config app
+const app = express()
+const PORT = 8000
+
+// connect db
+require('./models')
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// ROUTES/Controllers
+
+app.get('/', (req,res)=> {
+    res.json({ message: 'Welcome to Pokemon TCG Lost Origin API'})
+})
+
+app.use('/pokemoncards', require ('./controllers/pokemoncards'))
+
+// listen on a port
+
+app.listen(PORT, ()=> {
+    console.log(`Listening to Pokemon screams on port: ${PORT}`)
+})
