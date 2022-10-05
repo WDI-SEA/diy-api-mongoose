64fe7e79ec27557f33f6d08b6e98b6fe82dca395
valerieyang00 <valerieyang00@gmail.com>
2022-10-05 09:28:45 -0700

    clean codes

--- relationships.js
+++ relationships.js
@@ -2,23 +2,23 @@ const db = require('./models')
 
 const setCategories = async () => {
     try {
-        // const expense = await db.Expense.findOneAndUpdate({}) // only one expense right now
+        const expense = await db.Expense.findOneAndUpdate({}) // only one expense right now
 
-        // const groceries = await db.Category.findOne({ name: 'groceries' })
+        const groceries = await db.Category.findOne({ name: 'groceries' })
 
-        // expense.category = groceries
+        expense.category = groceries
         
-        // await expense.save()
+        await expense.save()
 
-        // const expenseInFull = await db.Expense.findOne({}).populate('category')
+        const expenseInFull = await db.Expense.findOne({}).populate('category')
 
-        // groceries.expenses.push(expense)
+        groceries.expenses.push(expense)
 
-        // groceries.save()
+        groceries.save()
 
         const categoryInFull = await db.Category.findOne({ name: 'groceries'}).populate('expenses')
 
-        // console.log(expenseInFull)
+        console.log(expenseInFull)
         console.log(categoryInFull)
 
     }catch(err) {
@@ -27,4 +27,4 @@ const setCategories = async () => {
 
 }
 
-setCategories()
+// setCategories()
