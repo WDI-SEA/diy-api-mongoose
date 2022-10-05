7cd1952bc56dd4567481e3643c3aab1c27bff7b8
aimeepham14 <aimee.pham14@gmail.com>
2022-10-04 21:37:56 -0700

    all routes work for boba api

--- controllers/boba.js
+++ controllers/boba.js
@@ -14,6 +14,18 @@ router.get('/', async (req, res) => {
 })
 
 
+//GET /boba/:id -- return a single boba drink
+router.get('/:id', async (req, res) => {
+    try{
+        const boba = await db.Boba.findById(req.params.id)
+        res.json(boba)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: 'internal server error '})
+    }
+})
+
+
 
 //POST /boba -- create a new boba drink in the db
 router.post('/', async (req, res) => {
@@ -25,5 +37,31 @@ router.post('/', async (req, res) => {
         res.status(500).json({ message: 'internal server error '})
     }
 })
+
+
+// PUT /boba/:id -- update a single boba drink
+router.put('/:id', async (req, res) => {
+    try{
+        const options = { new: true }
+        const updatedBoba = await db.Boba.findByIdAndUpdate(req.params.id, req.body, options)
+        res.json(updatedBoba)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: 'internal server error '})
+    }
+})
+
+
+//DELETE /boba/:id -- destroy a boba drink
+router.delete('/:id', async (req, res) => {
+    try{ 
+        await db.Boba.findByIdAndDelete(req.params.id)
+        res.sendStatus(204)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: 'internal server error '})
+    }
+})
+
 
 module.exports = router
