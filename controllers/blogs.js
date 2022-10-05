8f1b9994fbed431a97ce28e0d98c4d5d3e393c70
average-alien <allentang113@gmail.com>
2022-10-04 21:43:10 -0700

    express server up and running

---
+++ controllers/blogs.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const db = require('../models')
+
+const router = express.Router()
+
+// GET /blogs
+
+// POST /blogs
+
+// GET /blogs/:id
+
+// PUT /blogs/:id
+
+// DELETE /blogs/:id
+
+
+module.exports = router
