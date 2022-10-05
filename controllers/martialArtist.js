ead1b5ba67e3410ffea507ec8b3f8461e5d9d3ca
devin-lynch <devin.l.lynch@gmail.com>
2022-10-04 20:17:44 -0500

    Add get route

---
+++ controllers/martialArtist.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+
+// GET /martialartist -- return array of all martial artists
+router.get('/', async (req, res) => {
+    try {
+        // find all martial artists
+        const martialArtists = await db.MartialArtist.find({})
+        // send them back as json (with default status code 200)
+        res.json(martialArtists)
+    } catch(err) {
+        console.warn(err)
+        res.status(500).json({ message: 'internal server error' })          
+    }
+})
