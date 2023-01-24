b72a906eb49bffb3bdc3246766b070b3e144a5a8
fmher <feumatthewher2@gmail.com>
2023-01-24 00:29:06 -0800

    fin??

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = 8000
+
+require('./models')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'Welcome To the Blog '})
+})
+
+
+
+app.listen(PORT, () => console.log(`Blog on ${PORT}`))
